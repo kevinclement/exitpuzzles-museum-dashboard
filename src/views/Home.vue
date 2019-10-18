b8e202ea74bec3c3889302854827d496b84412dd
Kevin Clement <kevin.clement@gmail.com>
2019-10-18 12:07:11 -0700

    timer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,57 @@
 <template>
   <div class="home">
     <img class="logo" src="../assets/logo.png" />
+    <div class="countdown">CLOSES IN - {{prettyHours}} {{prettyMinutes}}</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'home'
+  name: 'home',
+
+  data () {
+    return {
+      hours: 2,
+      minutes: 11,
+    }
+  },
+  computed: {
+    prettyHours: function() {
+      return `${this.hours}h`
+    },
+    prettyMinutes: function() {
+      return `${this.minutes}m`
+    }
+  },
+  mounted() {
+    window.setInterval(() => {
+      if (this.minutes == 0) {
+        if (this.hours != 0) {
+          this.hours--;
+          this.minutes = 60
+        }
+      } else {
+        this.minutes--;
+      }
+      
+    }, 1000)
+  }
+    // show a disconnected bar if
 }
 </script>
 
 <style scoped>
   .logo {
-    width: 1100px;
-    
+    width: 657px;/*800*/
+  }
+  .countdown {
+    padding-top: 10px;
+    font-size: 85px;
+    opacity:.7;
+    font-family: 'BOOK ANTIQUA'
   }
   .home {
     background-image: url("../assets/background.png");
-    width: 1728px;
-    height: 979px;
     text-align:center;
     background-repeat: no-repeat; /* Do not repeat the image */
   
