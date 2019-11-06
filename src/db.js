51e95dbcd9d2b1f479f9b12b8590d28a80dde62d
Kevin Clement <kevin.clement@gmail.com>
2019-11-05 23:33:08 -0800

    add env for dev vs prod

--- src/db.js
+++ src/db.js
@@ -1,9 +1,16 @@
 import firebase from 'firebase/app'
 import 'firebase/database'
 
+const dbJSON = require('../db.json')
+
+let url = dbJSON.dev
+if (process.env.VUE_APP_DEV_MODE && process.env.VUE_APP_DEV_MODE == 'production') {
+  url = dbJSON.prod
+}
+
 // Get a Firestore instance
 export const db = firebase
-  .initializeApp({ databaseURL: require('../db.json').prod })
+  .initializeApp({ databaseURL: url })
   .database()
 
 export const ref = db.ref('museum').child('devices/dashboard')
